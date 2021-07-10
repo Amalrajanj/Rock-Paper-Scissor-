8f70e4734e2a0474bf3e8ec48cd3ccb50dfdbadd
anjsachu <amalrajnjanj@gmail.com>
2021-07-10 21:44:54 +0530

    initial

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-let score = document.querySelector('.score')
-let wrap = document.querySelector('.wrap')
-let match = document.querySelector('.match')
-let ResultStatus = document.querySelector('.ResultStatus')
-let userPickedItem = document.querySelectorAll('.user')
-let housePickedItem = document.querySelectorAll('.house')
-let rules = document.querySelector('.rules')
-let startedStatus = false
-let userChoice;
-let scoreValue= 0
-
+let choiceBtn = document.querySelectorAll('.p-w')
 let choice = ['paper','scissors','rock']
+let ResultStatus = document.querySelector('.ResultStatus')
+let score = document.querySelector('.score')
+let playWrap = document.querySelector('.play-wrap')
+let ResultWrap = document.querySelector('.result-wrap')
+let userPick = document.querySelectorAll('.userPick')
+let housePick = document.querySelectorAll('.housePick')
+let rule = document.querySelector('.rule')
 
-function randomChoice() {
-    return choice[Math.floor ( Math.random() * choice.length )]
-}
+let userChoice;
+let houseChoice;
+let scoreValue= 0;
 
-let userPicked = document.querySelectorAll('.user-picked')
 
-userPicked.forEach((data)=>{
-    data.addEventListener('click',()=>{
-        userChoice = data.getAttribute('data-userChoice')
-        startGame(userChoice)
-        
+
+choiceBtn.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        userChoice = item.getAttribute('data-userChoice')
+        startGame()
+        console.log(userChoice);
     })
 })
 
-function startGame(userChoice) {
-    housePicked = randomChoice()
-    console.log('You picked '+" "+userChoice);
-    console.log('House picked '+" "+ housePicked);
-    wrap.style.display = 'none'
-    userPickedItem[choice.indexOf(userChoice)].style.display = 'block'
-    housePickedItem[choice.indexOf(housePicked)].style.display = 'block'
-    match.style.display = 'flex'
-    if(userChoice==housePicked){
-        console.log('draw');
-        ResultStatus.innerText = 'DRAW'
+function houseRandom(){
+    return choice[Math.floor(Math.random() * choice.length)]
+}
+
+function startGame(){
+    houseChoice = houseRandom()
+    playWrap.style.display = 'none'
+    ResultWrap.style.display = 'grid'
+    housePick[choice.indexOf(houseChoice)].style.display = 'block'
+    userPick[choice.indexOf(userChoice)].style.display = 'block'
+    
+    if(userChoice==houseChoice){
+        ResultStatus.innerText = 'YOU DRAW'
     }else if(
-        userChoice == 'rock' && housePicked == 'scissors' ||
-        userChoice == 'scissors' && housePicked == 'paper' ||
-        userChoice == 'paper' && housePicked == 'rock'
+        userChoice == 'rock' && houseChoice == 'scissors' ||
+        userChoice == 'scissors' && houseChoice == 'paper' ||
+        userChoice == 'paper' && houseChoice == 'rock'
         ){
+            console.log('YOU WIN');
             ResultStatus.innerText = 'YOU WIN'
             updateValue(1)  
         }else{
+            console.log('YOU LOSE');
             ResultStatus.innerText = 'YOU LOSE'
             updateValue(-1)
         }
@@ -51,18 +52,16 @@ function startGame(userChoice) {
 
 function updateValue(value){
     scoreValue =scoreValue+value
-
+    console.log(scoreValue);
     score.innerText = scoreValue
 }
 function playAgain(){
-    wrap.style.display = 'flex'
-    match.style.display = 'none'
-    userPickedItem[choice.indexOf(userChoice)].style.display = 'none'
-    housePickedItem[choice.indexOf(housePicked)].style.display = 'none'
+    playWrap.style.display = 'grid'
+    ResultWrap.style.display = 'none'
 }
-function ShowRules(){
-    rules.style.display ='flex'
+function ruleOpen(){
+    rule.style.display = 'flex'
 }
-function closeRules(){
-    rules.style.display ='none'
+function ruleClose(){
+    rule.style.display = 'none'
 }
